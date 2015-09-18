7cf787cf1323509969297f10c7c44b5ea991a8dd
Jack Singleton <git@jacksingleton.com>
2015-09-17 23:24:12 -0700

    clean up package.js

--- packages/accounts-sandstorm-dev/package.js
+++ packages/accounts-sandstorm-dev/package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'accounts-sandstorm-dev',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Stubs out Sandstorm user headers and provides helper methods to stub them in client integration tests',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/sandforms/sandforms.git',
   documentation: 'README.md',
   debugOnly: true
 });
