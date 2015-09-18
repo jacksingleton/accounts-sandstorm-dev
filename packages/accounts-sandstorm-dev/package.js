f4ae17095361fcc0bb2c15a0078077fc2b539f40
Jack Singleton <git@jacksingleton.com>
2015-09-17 23:46:10 -0700

    fix parent packages, README, and correct repo in package.js

--- packages/accounts-sandstorm-dev/package.js
+++ packages/accounts-sandstorm-dev/package.js
@@ -1,9 +1,14 @@
 Package.describe({
-  name: 'accounts-sandstorm-dev',
-  version: '0.0.1',
-  summary: 'Stubs out Sandstorm user headers and provides helper methods to stub them in client integration tests',
-  git: 'https://github.com/sandforms/sandforms.git',
+  name: 'jacksingleton:accounts-sandstorm-dev',
+
+  version: '0.0.2',
+
+  summary: 'Stubs Sandstorm user headers and provides helper methods to control their values in tests',
+
+  git: 'https://github.com/jacksingleton/accounts-sandstorm-dev.git',
+
   documentation: 'README.md',
+
   debugOnly: true
 });
 
@@ -11,8 +16,6 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('http@1.1.0');
-
-  api.imply('kenton:accounts-sandstorm');
 
   api.addFiles('insert-stub-headers.js', 'server');
   api.addFiles('test-helpers.js', 'client');
